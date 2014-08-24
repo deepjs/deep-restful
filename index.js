6c4b67b80bb366e626acacec32f0e34258c7bec1
Gilles Coomans <gilles.coomans@gmail.com>
2014-08-24 21:38:05 +0200

    add restful units

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 if (typeof define !== 'function') {
     var define = require('amdefine')(module);
 }
-define(["require", "deepjs/deep", "./lib/collection", "./lib/chain"], function(require, deep, collection, chain){
+define(["require", "deepjs/deep", "./lib/chain"], function(require, deep, collection, chain){
+	deep.coreUnits = deep.coreUnits || [];
+	deep.coreUnits.push("js::deep-restful/tests/collections","js::deep-restful/tests/object","js::deep-restful/tests/restrictions");
 	return deep;
 });
